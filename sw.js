1ca25b2d5cf23cfd3022ea429b36f15caa8c48c3
metehankutlay <metehan.kutlay@gmail.com>
2026-03-29 23:20:14 +0300

    auto-deploy 23:20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'njox-v4';
-const ASSETS = ['./game.html', './manifest.json', './icon.png', './splash-icon.png', './kule.png'];
+const CACHE = 'njox-v7';
+const ASSETS = ['./game.html', './index.html', './manifest.json', './icon.png', './splash-icon.png', './kule.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -13,14 +13,15 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
+// Network-first: önce sunucudan çek, başarısız olursa cache'den göster
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       if(res.ok) {
         const c = res.clone();
         caches.open(CACHE).then(cache => cache.put(e.request, c));
       }
       return res;
-    }))
+    }).catch(() => caches.match(e.request))
   );
 });
